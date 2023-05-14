f792feb929fc00e0b96228b8030d7875ea623b3d
codewithdani <daneximpex@gmail.com>
2023-05-14 13:28:46 +0300

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    def new_in_list(my_list, idx, element):
     copy = my_list.copy()
     for x in my_list:
         if idx == my_list.index(x):
