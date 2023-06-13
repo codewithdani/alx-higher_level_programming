6aafe3e8e33d2c5b9d5254e83ccb3a8e317bfc63
codewithdani <118841134+codewithdani@users.noreply.github.com>
2023-06-13 17:31:10 +0300

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Module that contains a function that returns an object by a JSON representation """
+""" Module that contains a function that returns an object by 
+a JSON representation """
 import json
 
 
