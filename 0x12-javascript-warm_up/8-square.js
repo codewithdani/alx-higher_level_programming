6c7dd420ec5d36d1124a84705e2f6d9278da98d9
Daniel <daniel@gmail.com>
2023-08-14 22:01:20 +0300

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,8 +3,8 @@ const size = Math.floor(Number(process.argv[2]));
 
 if (!isNaN(size)) {
     for (let i = 0; i < size; i++) {
-        let line = "";
-        for (let j = 0; j < size; j++) line += "X"
+        let line = '';
+        for (let j = 0; j < size; j++) line += 'X';
         console.log(line);
     }
 } else {
