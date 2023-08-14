2765e182b1f56f88527cfbd1e1091b0b04d7efb2
Daniel <daniel@gmail.com>
2023-08-14 21:01:34 +0300

    5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const [, , argument] = process.argv;
+const number = parseInt(argument);
+
+if (!isNaN(number)) {
+    console.log(`My number: ${number}`);
+} else {
+    console.log("Not a number");
+}
