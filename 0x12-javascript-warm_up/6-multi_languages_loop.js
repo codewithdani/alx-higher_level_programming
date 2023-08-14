4e0d92cd0a0693cd4d39b72529e90c621c6b8f5c
Daniel <daniel@gmail.com>
2023-08-14 21:23:20 +0300

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-const lines = [
-    'C is fun',
-    'Python is cool',
-    'JavaScript is amazing'
-];
-
-for (const line of lines) {
-    console.log(line);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
