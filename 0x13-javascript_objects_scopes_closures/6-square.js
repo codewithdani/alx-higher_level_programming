5a6c4ad5da13fb746470a653b58dfe0da0c9ba5d
Daniel <daniel@gmail.com>
2023-08-15 21:24:57 +0300

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const ParentSquare = require('./5-square');
+module.exports = class Square extends ParentSquare {
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
