40a63db96bc86c448fb6278c17abe3686a8191de
Daniel <daniel@gmail.com>
2023-08-15 22:14:02 +0300

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
-const newList = list.map((value, index) => value * index);
-console.log('Initial List:', list);
-console.log('New List:', newList);
+consol.log(list);
+consol.log(list.map((value, index) => value * index));
