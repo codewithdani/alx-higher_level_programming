a6cdc1853f648f057d6eed4eb3338f1e7558e4b0
Daniel <daniel@gmail.com>
2023-08-18 18:38:39 +0300

    10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This script retrieves a State object by name from the hbtn_0e_6_usa database.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search_name = sys.argv[4]
+
+    # Connect to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username, password, database), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query the State object by name
+    state = session.query(State).filter(State.name == search_name).first()
+
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
+    # Close the session
+    session.close()
