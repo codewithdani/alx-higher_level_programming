343dabdf8a70f85900eec790331dcbfa9263aed3
Daniel <daniel@gmail.com>
2023-08-18 12:21:43 +0300

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-This script connects to a MySQL database and lists all states from the hbtn_0e_0_usa database.
-"""
+""" This script connects to a MySQL database and lists all states from the hbtn_0e_0_usa database."""
 import MySQLdb
 import sys
 
