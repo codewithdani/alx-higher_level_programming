d2ffeac0ffdf26606fee1097a9063ddeae179fe3
Daniel <daniel@gmail.com>
2023-08-18 19:07:22 +0300

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     # Connect to the database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         username, password, database), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
