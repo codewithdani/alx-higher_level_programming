f553b1ec82a80e5f1e1f2f6117fc8e9e03f347c3
Daniel <daniel@gmail.com>
2023-08-19 21:11:02 +0300

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-    A script that lists all states from the database hbtn_0e_0_usa
+    lists all states from the database hbtn_0e_0_usa
     Username, password and database names are given as user args
 """
 
@@ -11,12 +11,10 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    """ Retrieving command line arguments """
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    """ Connecting to the database """
     db = MySQLdb.connect(
         host="localhost",
         user=username,
@@ -26,19 +24,10 @@ if __name__ == "__main__":
         charset="utf8"
     )
 
-    """ Creating a cursor object to execute SQL queries """
     cursor = db.cursor()
-
-    """ Executing the SQL query """
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    """ Fetching all the results from the query """
     results = cursor.fetchall()
-
-    """ Printing the results """
     for row in results:
         print(row)
-
-    """ Closing the cursor and database connection """
     cursor.close()
     db.close()
