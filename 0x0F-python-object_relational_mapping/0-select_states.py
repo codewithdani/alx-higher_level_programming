b1cfbea74a46fa06d37a46ef7fc9a2f93523f3b1
Daniel <daniel@gmail.com>
2023-08-20 20:51:16 +0300

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,5 @@
-#!/usr/bin/python3
-"""
-    lists all states from the database hbtn_0e_0_usa
-    Username, password and database names are given as user args
-"""
-
-
+#!/usr/bin/pytho3
+"""  lists all states from the database hbtn_0e_0_usa """
 import sys
 import MySQLdb
 
