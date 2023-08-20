3b65b4f4ff26e95d28d2eff94a0517574b3ae93c
Daniel <daniel@gmail.com>
2023-08-21 02:05:02 +0300

    102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -6,7 +6,7 @@ from the hbtn_0e_101_usa database.
 import sys
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
