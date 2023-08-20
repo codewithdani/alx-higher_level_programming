daeadf6ac09e3003c5b8d9a8bac8bc6df03c5498
Daniel <daniel@gmail.com>
2023-08-21 00:53:01 +0300

    relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -2,7 +2,8 @@
 """
 This script defines the State class.
 """
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from relationship_city import Base, City
 
@@ -12,7 +13,6 @@ class State(Base):
     State class with attributes id, name, cities.
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete-orphan",
-                          backref="state")
+    cities = relationship("City", backref="states")
