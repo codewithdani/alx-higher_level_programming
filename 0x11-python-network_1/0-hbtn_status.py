5c959dcbcbd6c3db8c4391b6395b231556c769e9
Daniel <daniel@gmail.com>
2023-09-01 20:42:51 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,8 +8,8 @@ Fetches https://alx-intranet.hbtn.io/status and displays its body.
 if __name__ == "__main__":
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        body = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        body = res.read()
         print("Body response:")
         print("\t- type:", type(body))
         print("\t- content:", body.decode('utf-8'))
