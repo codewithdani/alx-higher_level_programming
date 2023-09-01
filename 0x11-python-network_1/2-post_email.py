2d5e6589bf1cf4b36ab753d5c625941070dd8243
Daniel <daniel@gmail.com>
2023-09-01 21:07:36 +0300

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to a URL
+with an email parameter and
+displays the response body.
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
