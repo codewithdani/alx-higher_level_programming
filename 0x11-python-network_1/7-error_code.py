89efea01e906ee600f816fccae8f40457686fc6c
Daniel <daniel@gmail.com>
2023-09-01 21:44:55 +0300

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Sends a request to a URL
+and displays the response body
+or handles HTTP error codes.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
